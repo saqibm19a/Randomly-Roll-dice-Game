1215693d0dd5c4d021af717dfae124dc08e042d7
saqibm19a <sm4179515@gmail.com>
2024-03-10 00:14:09 +0500

    change a image

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function rollDice() {
     let diceImages = [
-        'Image/dice (1).jpg',
-        'Image/dice (2).jpg',
-        'Image/dice (3).jpg',
-        'Image/dice (4).jpg',
-        'Image/dice (5).jpg',
-        'Image/dice (6).jpg'
+        'Image/dice (1).png',
+        'Image/dice (2).png',
+        'Image/dice (3).png',
+        'Image/dice (4).png',
+        'Image/dice (5).png',
+        'Image/dice (6).png'
     ];
 
     var randomIndex = Math.floor(Math.random() * diceImages.length);
@@ -17,10 +17,7 @@ function rollDice() {
 
 
 
-
-
-
-
+png
 
 
 
